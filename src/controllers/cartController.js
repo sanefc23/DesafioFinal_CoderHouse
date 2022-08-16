36109444407e17ee8c184b7de45d8c98f5525346
sanefc23 <san.efc@gmail.com>
2022-08-16 16:28:29 -0300

    fixed nodemailer + twilio

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,7 +1,7 @@
 const Cart = require('../models/Cart');
 const CartAPI = require('../APIs/cartsAPI')
 const UsersAPI = require('../APIs/usersAPI');
-const sendWPMessage = require('../services/twilio');
+// const sendWPMessage = require('../services/twilio');
 const sendEmail = require('../services/sendEmail');
 const config = require('../config/config');
 const logger = require('../services/logger');
@@ -94,11 +94,11 @@ const cartController = {
                     })
             })
     },
-    checkout: (req, res) => {
+    checkout: async (req, res) => {
         const cart = JSON.parse(req.cookies.userCart);
         UsersAPI.getByEmail(cart.user)
             .then(async userData => {
-                const content = `Hola ${userData.name}! Recibimos correctamente tu orden #${cart._id}.\nLo vas a estar recibiendo en tu domicilio ${userData.adress}.\nProductos:\n${cart.products.map(p => `${p.id_prod} x ${p.units}\n`)}`;
+                // const content = `Hola ${userData.name}! Recibimos correctamente tu orden #${cart._id}.\nLo vas a estar recibiendo en tu domicilio ${userData.adress}.\nProductos:\n${cart.products.map(p => `${p.id_prod} x ${p.units}\n`)}`;
                 const emailMessage = {
                     from: {
                         name: config.ETHEREAL_NAME,
@@ -112,10 +112,10 @@ const cartController = {
                         `,
                 }
                 try {
-                    await sendWPMessage(userData.phone, content)
+                    // await sendWPMessage(userData.phone, content)
                     await sendEmail(emailMessage)
                     res.status(200).json({
-                        whatsapp: content,
+                        // whatsapp: content,
                         email: emailMessage
                     })
                 } catch (error) {
